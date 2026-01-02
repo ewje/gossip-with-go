b7956fb86fa7a520252edf94ed5286fc18e8efaf
ewje <ethanwje@gmail.com>
2026-01-02 21:21:11 +0800

    edits

--- go/internal/router/router.go
+++ go/internal/router/router.go
@@ -4,6 +4,7 @@ import (
 	"github.com/ewje/gossip-with-go/internal/routes"
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 )
 
 func Setup() chi.Router {
@@ -14,13 +15,22 @@ func Setup() chi.Router {
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.AllowContentType("application/json"))
 
+	r.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
 	setUpRoutes(r)
 	return r
 }
 
 func setUpRoutes(r chi.Router) {
-	// Groups all routes under /api based on your initial design,
-	// or matches the sample app's root grouping.
 	r.Route("/api", func(r chi.Router) {
 		r.Group(routes.GetRoutes())
 	})
